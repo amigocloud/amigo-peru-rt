3628ab0093a011da1b0943dbd4454e5b738078cd
Javier Villafuerte <jvilalf@gmail.com>
2020-04-27 12:35:54 -0500

    updated store and api localhost url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://realtime-r0-covid-brazil.herokuapp.com',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
