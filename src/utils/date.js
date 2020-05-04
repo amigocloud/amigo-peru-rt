3ba8bc37999e5058b92306b6d365592549f32899
Ragi Yaser Burhum <ragi@amigocloud.com>
2020-05-03 20:27:15 -0500

    Change date to Spanish

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,5 @@
 export const formatDate = (date, options) =>
-  new Intl.DateTimeFormat('pt-BR', options).format(date);
+  new Intl.DateTimeFormat('es-ES', options).format(date);
 
 export const fullFormatDate = (date) =>
   formatDate(date, { month: 'long', day: 'numeric', year: 'numeric' });
